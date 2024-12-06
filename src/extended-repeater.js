56dc6d22c75726ec4dd69cd8d9df9ffbe88dabf8
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2024-12-06 21:10:08 +0100

    feat: pass 48 tests

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  for (let i = 1; i <= options.repeatTimes; i += 1){
+    let secondPart = '';
+    for (let j = 1; j <= options.additionRepeatTimes; j += 1){
+      if (!options.additionSeparator) {
+        options.additionSeparator = '|';
+      }
+      if (j === options.additionRepeatTimes){
+        secondPart += options.addition;
+      } else {
+        secondPart += options.addition + options.additionSeparator;
+      }
+    }
+    if (!options.separator) {
+      options.separator = '+';
+    }
+    if (options.addition && secondPart === ''){
+      secondPart = options.addition;
+    }
+    if (i === options.repeatTimes) {
+      result += str + secondPart;
+    } else {
+      result += str + secondPart + options.separator;
+    }
+  }
+  return result;
 }
 
 module.exports = {
