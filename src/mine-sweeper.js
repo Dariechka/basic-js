e08ca5b2732e30f0ed5e2a7a9337d6a6bdb3db09
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2024-12-07 17:19:45 +0100

    feat: pass 53 tests

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix.length; i += 1){
+    let preResult = [];
+    for ( let j = 0; j < matrix[i].length; j += 1){
+      let value = 0;
+      if (matrix[i + 1] !== undefined && matrix[i + 1][j] === true){
+        value += 1;
+      }
+      if (matrix[i - 1] !== undefined && matrix[i - 1][j] === true){
+        value += 1;
+      }
+      if (matrix[i][j + 1] !== undefined && matrix[i][j + 1] === true){
+        value += 1;
+      }
+      if (matrix[i][j - 1] !== undefined && matrix[i][j - 1] === true){
+        value += 1;
+      }
+      if (matrix[i + 1] !== undefined && matrix[i + 1][j + 1] !== undefined && matrix[i + 1][j + 1] === true){
+        value += 1;
+      }
+      if (matrix[i + 1] !== undefined && matrix[i + 1][j - 1] !== undefined && matrix[i + 1][j - 1] === true){
+        value += 1;
+      }
+      if (matrix[i - 1] !== undefined && matrix[i - 1][j + 1] !== undefined && matrix[i - 1][j + 1] === true){
+        value += 1;
+      }
+      if (matrix[i - 1] !== undefined && matrix[i - 1][j - 1] !== undefined && matrix[i - 1][j - 1] === true){
+        value += 1;
+      }
+      preResult.push(value);
+    }
+    result.push(preResult);
+  }
+  return result;
 }
 
 module.exports = {
