c302c1346559d41d1753c2e5b4b475b314f25991
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2024-12-08 15:48:38 +0100

    feat: pass 74 tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr.length === 0){
+    return [];
+  }
+  let result = [...arr];
+  for (let i = 0; i < result.length; i += 1){
+    if (result[i] === '--double-next'){
+      if (typeof(result[i + 1] === 'object')){
+        result[i] = Object.assign({}, result[i + 1]);
+      }
+      (result[i + 1] !== undefined) ? result.splice(i, 1, result[i +1]) : result.splice(i, 1);
+    }
+    if (result[i] === '--double-prev'){
+      if (typeof(result[i - 1] === 'object')){
+        result[i] = Object.assign({}, result[i - 1]);
+      }
+      (result[i - 1] !== undefined) ? result.splice(i, 1, result[i - 1]) : result.splice(i, 1);
+    }
+    if (result[i] === '--discard-prev'){
+      if (result[i - 1] === undefined){
+        result.splice(i, 1);
+      } else {
+        result[i - 1] = result[i - 1].toString() + "DISCARD";
+        result[i] = result[i] + "DISCARD";
+      }
+    }
+    if (result[i] === '--discard-next'){
+      if (result[i + 1] === undefined){
+        result.splice(i, 1);
+      } else {
+        result[i + 1] = result[i + 1].toString() + "DISCARD";
+        result[i] = result[i] + "DISCARD";
+      }
+    }
+  }
+  for (let i = 0; i < result.length; i += 1){
+  if (typeof(result[i]) === 'string' && result[i].indexOf('DISCARD') !== -1){
+    result.splice(i, 1);
+    i -= 1;
+  }
+}
+  return result;
 }
 
 module.exports = {
